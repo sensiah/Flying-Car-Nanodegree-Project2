aa18c93b1e89fd73ab6f27b09555657e835046d6
Sid Ahmed SENDJEL <57562642+sensiah@users.noreply.github.com>
2021-05-17 13:50:58 +0100

    Update planning_utils.py

--- planning_utils.py
+++ planning_utils.py
@@ -1,27 +1,29 @@
 from enum import Enum
 from queue import PriorityQueue
 import numpy as np
+from bresenham import bresenham
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = [20, 20]
 
 
 def create_grid(data, drone_altitude, safety_distance):
-    """
-    Returns a grid representation of a 2D configuration space
-    based on given obstacle data, drone altitude and safety distance
-    arguments.
-    """
 
     # minimum and maximum north coordinates
     north_min = np.floor(np.min(data[:, 0] - data[:, 3]))
     north_max = np.ceil(np.max(data[:, 0] + data[:, 3]))
+    print('North min: {} \tNorth max: {}'.format(north_min, north_max))
 
     # minimum and maximum east coordinates
     east_min = np.floor(np.min(data[:, 1] - data[:, 4]))
     east_max = np.ceil(np.max(data[:, 1] + data[:, 4]))
+    print('East min: {} \tEast max: {}'.format(east_min, east_max))
 
     # given the minimum and maximum coordinates we can
     # calculate the size of the grid.
     north_size = int(np.ceil(north_max - north_min))
     east_size = int(np.ceil(east_max - east_min))
+    print('North size: {} \tEast size: {}'.format(north_size, east_size))
 
     # Initialize an empty grid
     grid = np.zeros((north_size, east_size))
@@ -45,6 +47,7 @@ def create_grid(data, drone_altitude, safety_distance):
 class Action(Enum):
     """
     An action is represented by a 3 element tuple.
+
     The first 2 values are the delta of the action relative
     to the current grid position. The third and final value
     is the cost of performing the action.
@@ -54,11 +57,10 @@ class Action(Enum):
     EAST = (0, 1, 1)
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
-
-    NORTH_EAST = (-1, 1, 1.14) #cost of sqrt(2)
-    NORTH_WEST = (-1, -1, 1.14)
-    SOUTH_EAST = (1, 1, 1.14)
-    SOUTH_WEST = (1, -1, 1.14)
+    NORTH_WEST = (-1, -1, np.sqrt(2))
+    NORTH_EAST = (-1, 1, np.sqrt(2))
+    SOUTH_WEST = (1, -1, np.sqrt(2))
+    SOUTH_EAST = (1, 1, np.sqrt(2))
 
     @property
     def cost(self):
@@ -111,16 +113,16 @@ def a_star(grid, h, start, goal):
 
     branch = {}
     found = False
-    
+
     while not queue.empty():
         item = queue.get()
         current_node = item[1]
         if current_node == start:
             current_cost = 0.0
-        else:              
+        else:
             current_cost = branch[current_node][0]
-            
-        if current_node == goal:        
+
+        if current_node == goal:
             print('Found a path.')
             found = True
             break
@@ -131,12 +133,12 @@ def a_star(grid, h, start, goal):
                 next_node = (current_node[0] + da[0], current_node[1] + da[1])
                 branch_cost = current_cost + action.cost
                 queue_cost = branch_cost + h(next_node, goal)
-                
-                if next_node not in visited:                
-                    visited.add(next_node)               
+
+                if next_node not in visited:
+                    visited.add(next_node)
                     branch[next_node] = (branch_cost, current_node, action)
                     queue.put((queue_cost, next_node))
-             
+
     if found:
         # retrace steps
         n = goal
@@ -149,45 +151,91 @@ def a_star(grid, h, start, goal):
     else:
         print('**********************')
         print('Failed to find a path!')
-        print('**********************') 
+        print('**********************')
     return path[::-1], path_cost
-
 
 
 def heuristic(position, goal_position):
     return np.linalg.norm(np.array(position) - np.array(goal_position))
 
-### PATH SHORTENINNG SECTION ###
-
-epsilon = 5
 
 def point(p):
     return np.array([p[0], p[1], 1.]).reshape(1, -1)
 
-def collinearity_check(p1, p2, p3, epsilon):   
+
+def collinearity_check(p1, p2, p3, epsilon=1e-6):
     m = np.concatenate((p1, p2, p3), 0)
     det = np.linalg.det(m)
-    return abs(det) < epsilon    
+    return np.abs(det) < epsilon
 
-def shorten_path(path):
-    waypoints = [p for p in path] #makes a copy of the 'path' list    
-    
+
+def prune_path(path):
+    if path is not None:
+        pruned_path = [p for p in path]
+        # TODO: prune the path!
+        i = 0
+        while i < len(pruned_path) - 2:
+            p1 = point(pruned_path[i])
+            p2 = point(pruned_path[i + 1])
+            p3 = point(pruned_path[i + 2])
+
+            # If the 3 points are in a line remove
+            # the 2nd point.
+            # The 3rd point now becomes and 2nd point
+            # and the check is redone with a new third point
+            # on the next iteration.
+            if collinearity_check(p1, p2, p3):
+                # Something subtle here but we can mutate
+                # `pruned_path` freely because the length
+                # of the list is check on every iteration.
+                pruned_path.remove(pruned_path[i + 1])
+            else:
+                i += 1
+
+        return pruned_path
+
+def bres_prune(grid, path):
+    """
+    Use the Bresenham module to trim uneeded waypoints from path
+    """
+    pruned_path = [p for p in path]
     i = 0
-    while i < len(waypoints) - 2:
-        p1 = point(waypoints[i])
-        p2 = point(waypoints[i+1])
-        p3 = point(waypoints[i+2])
-        
-        # If the 3 points are in a line remove
-        # the 2nd point.
-        # The 3rd point now becomes and 2nd point
+    while i < len(pruned_path) - 2:
+        p1 = pruned_path[i]
+        p2 = pruned_path[i + 1]
+        p3 = pruned_path[i + 2]
+        # if the line between p1 and p2 doesn't hit an obstacle
+        # remove the 2nd point.
+        # The 3rd point now becomes the 2nd point
         # and the check is redone with a new third point
         # on the next iteration.
-        if collinearity_check(p1, p2, p3, epsilon):
+        if  all((grid[pp] == 0) for pp in bresenham(int(p1[0]), int(p1[1]), int(p3[0]), int(p3[1]))):
             # Something subtle here but we can mutate
             # `pruned_path` freely because the length
-            # of the list is check on every iteration.
-            waypoints.remove(waypoints[i+1])
+            # of the list is checked on every iteration.
+            pruned_path.remove(p2)
+
         else:
             i += 1
-    return waypoints
+    return pruned_path
+
+
+def plot_chart_route(grid, path, start_ne, goal_ne, pruned):
+
+    fig = plt.figure()
+    plt.imshow(grid, origin='lower', cmap='Greys')
+
+    for i in range(len(path) - 1):
+        p1 = path[i]
+        p2 = path[i + 1]
+        plt.plot([p1[1], p2[1]], [p1[0], p2[0]], 'r-')
+
+    plt.plot(start_ne[1], start_ne[0], 'gx', markersize=15)
+    plt.plot(goal_ne[1], goal_ne[0], 'gx', markersize=15)
+
+    plt.xlabel('EAST', fontsize=20)
+    plt.ylabel('NORTH', fontsize=20)
+    if pruned:
+        fig.savefig('charts/grid_route_pruned.png', bbox_inches='tight')
+    else:
+        fig.savefig('charts/grid_route.png', bbox_inches='tight')
